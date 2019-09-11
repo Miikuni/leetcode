bd21904822ba605d72b05c8891f766175e85c8eb
Miikuni <48979243+Miikuni@users.noreply.github.com>
2019-09-11 10:52:48 +0800

    addTwoNumbers01版
    给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
    如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
    
    您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
    
    示例：
    
    输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
    输出：7 -> 0 -> 8
    原因：342 + 465 = 807
    
    01版没有解决满10进1问题

--- addTwoNumbers01.kt
+++ addTwoNumbers01.kt
@@ -5,25 +5,14 @@ class Solution {
     val resultListNode=ListNode(0)
     var step:ListNode? = resultListNode;
     var value = 0
-    var up = 0
     while (li1 != null || li2 != null){
         var numli1 = li1?.`val`?:0
         var numli2 = li2?.`val`?:0
-        var sum = numli1 + numli2 + up
-        if (sum>=10){
-            up = 1
-            value = sum%10
-        }else{
-            up = 0
-            value =sum
-        }
+        var sum = numli1 + numli2 
         step!!.next = ListNode(value)
         step = step.next
         if (li1!=null)li1 = li1?.next
         if (li2!=null)li2 = li2?.next
-    }
-    if (up>0){
-        step!!.next = ListNode(up)
     }
     return resultListNode.next
 }
